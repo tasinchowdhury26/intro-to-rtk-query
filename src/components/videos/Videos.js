f2962b3f9aea4c6c5ec6e653aced4f077dfd29c3
tasinchowdhury26 <tasinchowdhury26@gmail.com>
2023-04-27 00:35:51 +0600

    finished

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,15 +1,22 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
 import Video from "./Video";
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-        </>
-    );
+  const {
+    data: videos,
+    isLoading,
+    isError,
+  } = useGetVideosQuery(undefined, {
+    skip: false,
+  });
+  let content;
+  if (isLoading) content = <VideoLoader />;
+  if (!isLoading && isError) content = <Error message="There was an error" />;
+  if (!isLoading && !isError && videos?.length === 0)
+    content = <Error message="No videos found!" />;
+  if (!isLoading && !isError && videos?.length > 0)
+    content = videos.map((video) => <Video key={video.id} video={video} />);
+  return content;
 }
